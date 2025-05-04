4dfcf6baa5b6d2ca8cbaf015ee2e6e969915f822
Benjamin Walkenhorst <krylon@posteo.de>
2025-05-04 19:07:53 +0200

    Add  __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-05-04 19:07:47 krylon>
+#
+# /data/code/python/mesopotamia/__init__.py
+# created on 04. 05. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of mesopotamia. It is distributed under the terms
+# of the GNU General Public License 3. See the file LICENSE for
+# details or find a copy online at
+# https://www.gnu.org/licenses/gpl-3.0
+
+"""
+mesopotamia.__init__
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
